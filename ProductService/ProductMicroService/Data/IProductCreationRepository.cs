7621cace330b92e36ded4b91d09961b9c20488e5
NebojsaIvancevic <nebojsa.ivancevicit@gmail.com>
2021-09-07 20:24:43 +0200

    product-service: Added Model and Repo for Product

---
+++ ProductService/ProductMicroService/Data/IProductCreationRepository.cs
@@ -0,0 +1,21 @@
+﻿using ProductMicroService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductMicroService.Data
+{
+    public interface IProductCreationRepository
+    {
+        List<ProductCreationModel> GetProducts(string productName = null, string productPrice = null, int productQuantity = 0);
+        ProductCreationModel GetProductById(Guid productId);
+
+        ProductCreationConfirmation CreateProduct(ProductCreationModel productCreation);
+
+        ProductCreationConfirmation UpdateProduct(ProductCreationModel productCreation);
+
+
+        void DeleteProduct(Guid productId);
+    }
+} 
---
+++ ProductService/ProductMicroService/Data/ProductCreationRepository.cs
@@ -0,0 +1,36 @@
+﻿using ProductMicroService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductMicroService.Data
+{
+    public class ProductCreationRepository : IProductCreationRepository
+    {
+        public ProductCreationConfirmation CreateProduct(ProductCreationModel productCreation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteProduct(Guid productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProductCreationModel GetProductById(Guid productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProductCreationModel> GetProducts(string productName = null, string productPrice = null, int productQuantity = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProductCreationConfirmation UpdateProduct(ProductCreationModel productCreation)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
